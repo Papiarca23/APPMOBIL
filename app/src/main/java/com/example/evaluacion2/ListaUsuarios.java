71b56d12d8bc6681df30978e131ee7f0fe8fef02
Bastian Pavez <bpavez03@gmail.com>
2024-11-15 00:55:26 -0300

    cambio nombre listar dispositivos a listar usuarios

--- app/src/main/java/com/example/evaluacion2/ListaUsuarios.java
+++ app/src/main/java/com/example/evaluacion2/ListaUsuarios.java
@@ -8,13 +8,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class ListaDispositivos extends AppCompatActivity {
+public class ListaUsuarios extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_lista_dispositivos);
+        setContentView(R.layout.activity_listar_usuarios);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.Login), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
